318f80bf9bde8d8f6fdf533ec539ca79f3dc010a
Tufan-Kafle <pranayakafle@isu.edu.com>
2026-03-30 16:17:29 -0600

    Added integration test for stats endpoint to achieve 100% feature coverage

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from app.modules.jobs.routes import router as jobs_router
+from app.modules.stats.routes import router as stats_router
 
 app = FastAPI(title = "Job Search API",
               description="CS 3321 Project - Job Search API",
               version="1.0.0")
 
 # Include the jobs router
+app.include_router(stats_router, prefix="/stats", tags=["stats"])
 app.include_router(jobs_router, prefix="/jobs", tags=["jobs"])
 
 @app.get("/")
